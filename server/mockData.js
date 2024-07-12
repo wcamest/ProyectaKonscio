b48f521b6751c6eed3e66f7138cb53b932f535ff
William Estrada <willkmiloeo1228@gmail.com>
2024-07-12 07:51:42 -0500

    updates

--- server/mockData.js
+++ server/mockData.js
@@ -11,26 +11,20 @@ import NT795IFSIK4E1S7 from "./mock/NT795IFSIK4E1S7.json" with { type: "json" };
 import PKN1XPQGCWOL6AE from "./mock/PKN1XPQGCWOL6AE.json" with { type: "json" };
 import RI3P80G4EU8S4YV from "./mock/RI3P80G4EU8S4YV.json" with { type: "json" };
 import VMFTLJGQI5TMFEI from "./mock/VMFTLJGQI5TMFEI.json" with { type: "json" };
-import A8NGTHU0IT2MC2G from "./mock/A8NGTHU0IT2MC2G.json" with { type: "json" };
-import Y2WE1P5RP7V03WZ from "./mock/Y2WE1P5RP7V03WZ.json" with { type: "json" };
 
 const mockData = {
   empty,
   QHRZSBZUEIHSM4A,
   TDJWQ2FLUM4VO2C,
-
-  V80EGPO3ZHM43Q6, // Economia Real Colaborativa
-  MW7NVOJWM4XIWI1, // Educación Alternativa
-  A24U8W4F5YXR0V1, // Cultura Inclusiva
-  KV8RSB8Y6COVWIB, // Conciencia Ambiental
-  KVLBA3CL5Z445F6, // Innovaciones Tecnológicas
-  NT795IFSIK4E1S7, // Periodismo Digno
-  PKN1XPQGCWOL6AE, // Política de Conciencia
-  RI3P80G4EU8S4YV, // Historia no Manipulada
-  VMFTLJGQI5TMFEI, // Salud Y Bienestar
-
-  A8NGTHU0IT2MC2G, // Colabora
-  Y2WE1P5RP7V03WZ, // WebRTC y Espacios 3D
+  V80EGPO3ZHM43Q6,
+  MW7NVOJWM4XIWI1,
+  A24U8W4F5YXR0V1,
+  KV8RSB8Y6COVWIB,
+  KVLBA3CL5Z445F6,
+  NT795IFSIK4E1S7,
+  PKN1XPQGCWOL6AE,
+  RI3P80G4EU8S4YV,
+  VMFTLJGQI5TMFEI
 };
 
 export default mockData;
